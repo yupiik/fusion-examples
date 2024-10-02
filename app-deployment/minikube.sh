9da1f47811c66b6f3a7c2e88bb7def463a5a07dd
francois papon <francois.papon@openobject.fr>
2024-10-02 10:21:13 +0200

    [deployment] Update minikube script

--- app-deployment/minikube.sh
+++ app-deployment/minikube.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
-
 set -e
 
-minikube status &> /dev/null || echo "[INFO] Starting minikube" && minikube start
+## Start minikube before
+#minikube status &> /dev/null || echo "[INFO] Starting minikube" && minikube start
 
 echo "[INFO] Building the image in minikube env"
+cd ..
 eval $(minikube docker-env) && \
-mvn clean package -DskipTests -pl "$(dirname $0)/../app-backend" jib:dockerBuild
+mvn clean package -DskipTests -pl "app-backend" jib:dockerBuild
 
 echo "[INFO] Deploying"
-mvn -pl "$(dirname $0)" bundlebee:apply@k8s
+mvn -pl "app-deployment" bundlebee:apply@k8s
 
 echo "[INFO] Enabling metrics-server"
 minikube addons enable metrics-server
 
 echo "[INFO] Starting dashboard"
 minikube dashboard
+
+## Show and open all observability consoles
+#minikube service --all -n observability
